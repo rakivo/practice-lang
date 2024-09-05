7288b93f60793c03599de871f53cbbf182673507
rakivo <awfulmarco@gmail.com>
2024-09-05 21:24:41 +0300

    Document `src/nob.c`

--- src/nob.c
+++ src/nob.c
@@ -1,25 +1,5 @@
-// A little subset of <https://github.com/tsoding/musializer/blob/master/nob.h>, just to run a sync command.
-
-// Copyright 2023 Alexey Kutepov <reximkut@gmail.com>
-//
-// Permission is hereby granted, free of charge, to any person obtaining
-// a copy of this software and associated documentation files (the
-// "Software"), to deal in the Software without restriction, including
-// without limitation the rights to use, copy, modify, merge, publish,
-// distribute, sublicense, and/or sell copies of the Software, and to
-// permit persons to whom the Software is furnished to do so, subject to
-// the following conditions:
-//
-// The above copyright notice and this permission notice shall be
-// included in all copies or substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
-// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
-// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
-// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+// A small subset of <https://github.com/tsoding/musializer/blob/master/nob.h>, just to run a sync command.
+// This C file is just a stitched parts of the `nob.h`, but `nob_cmd_run_async()` and `nob_cmd_run_sync()` modified to accept `bool echo` argument.
 
 #include "nob.h"
 
