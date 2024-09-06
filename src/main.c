8ef214e51466850edf9ff42930a3275f965ada27
rakivo <awfulmarco@gmail.com>
2024-09-06 16:45:58 +0300

    Group up things in `src/main.c`

--- src/main.c
+++ src/main.c
@@ -34,74 +34,9 @@ main_deinit(void)
 	memory_release();
 }
 
-UNUSED
-void print_elapsed(clock_t start, clock_t end, const char *what)
+static void
+check_for_main_function(const char *file_path)
 {
-	double elapsed = (double) (end - start) / CLOCKS_PER_SEC;
-	printf("%s took: %.0fus\n", what, elapsed * 1000000);
-}
-
-int
-main(int argc, const char *argv[])
-{
-	if (argc < 2) {
-		eprintf("Usage: %s <file_path>\n", argv[0]);
-		exit(1);
-	}
-
-	const char *file_path = argv[1];
-
-	const file_t file_ = new_file_t(new_str_t(file_path));
-	append_file(file_);
-
-	FILE *file = fopen(file_path, "r");
-	if (file == NULL) {
-		eprintf("error: failed to open file: %s\n", file_path);
-		return EXIT_FAILURE;
-	}
-
-	memory_init(3);
-
-	char line[LINE_CAP];
-	lines = (lines_t) malloc(sizeof(lines_t *) * LINES_CAP);
-	while (fgets(line, sizeof(line), file) != NULL) {
-		lines[lines_count++] = split(line, ' ');
-	}
-
-	fclose(file);
-
-	Lexer lexer = new_lexer(file_.file_id, lines, lines_count);
-
-#ifdef DEBUG
-	const clock_t lex_start = clock();
-#endif
-	tokens = lexer_lex(&lexer);
-#ifdef DEBUG
-	const clock_t lex_end = clock();
-	dbg_time(lex, "lexing");
-#endif
-
-#ifdef PRINT_TOKENS
-	for (size_t i = 0; i < lexer.tokens_count; ++i) printf("%s\n", token_to_str(&tokens[i]));
-#endif
-
-	Parser parser = new_parser(tokens, lexer.tokens_count);
-
-#ifdef DEBUG
-	const clock_t parser_start = clock();
-#endif
-	parser_parse(&parser);
-#ifdef DEBUG
-	const clock_t parser_end = clock();
-	dbg_time(parser, "parsing");
-#endif
-
-#ifdef PRINT_ASTS
-	for (ast_id_t i = 0; i < asts_len; ++i) print_ast(&astid(i));
-#endif
-
-	if (asts_len == 0) goto ret;
-
 	main_function_check(true, astid(0));
 	if(main_function == -1) {
 		report_error("%s:0:0: no main at top level function found", file_path);
@@ -114,12 +49,18 @@ main(int argc, const char *argv[])
 								 "  note: expected signature: func int do <...> end",
 								 loc_to_str(&locid(astid(main_function).loc_id)));
 	}
+}
 
+static void
+consteval_step(void)
+{
 	ast_t ast = astid(0);
 	Consteval consteval = new_consteval(&const_map);
+
 #ifdef DEBUG
 	const clock_t consteval_start = clock();
 #endif
+
 	while (ast.next && ast.next <= ASTS_SIZE) {
 		if (ast.ast_kind == AST_CONST) {
 			const consteval_value_t value = consteval_eval(&consteval, &ast, false);
@@ -135,25 +76,102 @@ main(int argc, const char *argv[])
 	const clock_t consteval_end = clock();
 	dbg_time(consteval, "constevaling");
 #endif
+}
 
+typedef struct {
+	tokens_t tokens;
+	size_t tokens_count;
+} ttc_t;
+
+static ttc_t
+lex_step(const char *file_path)
+{
+	const file_t file = new_file_t(new_str_t(file_path));
+	append_file(file);
+
+	FILE *stream = fopen(file_path, "r");
+	if (stream == NULL) {
+		eprintf("error: failed to open file: %s\n", file_path);
+		exit(EXIT_FAILURE);
+	}
+
+	char line[LINE_CAP];
+	lines = (lines_t) malloc(sizeof(lines_t *) * LINES_CAP);
+	while (fgets(line, sizeof(line), stream) != NULL) {
+		lines[lines_count++] = split(line, ' ');
+	}
+
+	fclose(stream);
+	Lexer lexer = new_lexer(file.file_id, lines, lines_count);
+
+#ifdef DEBUG
+	const clock_t lex_start = clock();
+#endif
+
+	const tokens_t tokens = lexer_lex(&lexer);
+
+#ifdef PRINT_TOKENS
+	for (size_t i = 0; i < lexer.tokens_count; ++i) printf("%s\n", token_to_str(&tokens[i]));
+#endif
+
+#ifdef DEBUG
+	const clock_t lex_end = clock();
+	dbg_time(lex, "lexing");
+#endif
+
+	return (ttc_t) {
+		.tokens = tokens,
+		.tokens_count = lexer.tokens_count
+	};
+}
+
+static void
+parse_step(const tokens_t tokens, size_t tokens_count)
+{
+	Parser parser = new_parser(tokens, tokens_count);
+
+#ifdef DEBUG
+	const clock_t parser_start = clock();
+#endif
+
+	parser_parse(&parser);
+
+#ifdef PRINT_ASTS
+	for (ast_id_t i = 0; i < asts_len; ++i) print_ast(&astid(i));
+#endif
+
+#ifdef DEBUG
+	const clock_t parser_end = clock();
+	dbg_time(parser, "parsing");
+#endif
+}
+
+static void
+compile_step(void)
+{
 	Compiler compiler = new_compiler(main_function, const_map, var_map);
 
 #ifdef DEBUG
 	const clock_t compile_start = clock();
 #endif
+
 	compiler_compile(&compiler);
 
 #ifdef DEBUG
 	const clock_t compile_end = clock();
 	dbg_time(compile, "compiling");
-	print_elapsed(lex_start, compile_end, "everything");
 #endif
+}
+
+static void
+compile_asm_step(void)
+{
+	Nob_Cmd cmd = {0};
 
 #ifdef DEBUG
 	const clock_t compile_asm_start = clock();
 #endif
 
-	Nob_Cmd cmd = {0};
 	nob_cmd_append(&cmd, PATH_TO_ASM_EXECUTABLE, X86_64_OUTPUT, ASM_FLAGS);
 	nob_cmd_run_sync(cmd, dbg_echo);
 	cmd.count = 0;
@@ -170,8 +188,35 @@ main(int argc, const char *argv[])
 	dbg_time(compile_asm, "compiling asm");
 #endif
 
-ret:
 	nob_cmd_free(cmd);
+}
+
+UNUSED
+void print_elapsed(clock_t start, clock_t end, const char *what)
+{
+	double elapsed = (double) (end - start) / CLOCKS_PER_SEC;
+	printf("%s took: %.0fus\n", what, elapsed * 1000000);
+}
+
+int
+main(int argc, const char *argv[])
+{
+	if (argc < 2) {
+		eprintf("Usage: %s <file_path>\n", argv[0]);
+		exit(1);
+	}
+
+	const char *file_path = argv[1];
+	memory_init(3);
+	const ttc_t ttc = lex_step(file_path);
+	parse_step(ttc.tokens, ttc.tokens_count);
+	if (asts_len == 0) goto ret;
+	check_for_main_function(file_path);
+	consteval_step();
+	compile_step();
+	compile_asm_step();
+
+ret:
 	main_deinit();
 	return 0;
 }
